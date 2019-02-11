5a4f1e499ef7915e2eda2043b3914120ce12e176
HoNo135 <hono70164@gmail.com>
2019-02-12 02:10:32 +0900

    Update 호노봇.py

--- 호노봇.py
+++ 호노봇.py
@@ -83,5 +83,5 @@ async def on_message(message):
 
         await client.send_message(channel, text)
 
-access_token = os.environ["BOT_TOKEN]
+access_token = os.environ["BOT_TOKEN"]
 client.run(access_token)
